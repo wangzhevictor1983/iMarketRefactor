e3ba32dd689a10826c934846fab18635a6b52da4
wangzhevictor1983 <wangzhevictor1983@outlook.com>
2018-01-04 13:51:51 +0800

    base project structure

---
+++ imarket/src/main/java/com/helpe/imarket/ImarketApplication.java
@@ -0,0 +1,12 @@
+package com.helpe.imarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImarketApplication.class, args);
+	}
+}
