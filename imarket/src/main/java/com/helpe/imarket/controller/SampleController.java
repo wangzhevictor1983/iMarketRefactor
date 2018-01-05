a3870dcad0479517e9950bbe88328ec10541c555
Wangz1 <Wangz1@LNGSHAL-224.legal.regn.net>
2018-01-05 14:05:25 +0800

    log sample

--- imarket/src/main/java/com/helpe/imarket/controller/SampleController.java
+++ imarket/src/main/java/com/helpe/imarket/controller/SampleController.java
@@ -2,6 +2,9 @@ package com.helpe.imarket.controller;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,11 +15,14 @@ import com.helpe.imarket.model.*;
 @RestController
 public class SampleController {
 
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 	private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping(value="/sample", method=RequestMethod.GET)
     public SampleModel sample(@RequestParam(value="name", defaultValue="World") String name) {
+    	logger.info("TEST");
         return new SampleModel(counter.incrementAndGet(),
                             String.format(template, name));
     }
